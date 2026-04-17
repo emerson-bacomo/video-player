6daba149c5381f2ef1ba0cbd12dadc8bfefe40b5
emerson-bacomo <ccs.emersonb@gmail.com>
2026-04-18 06:35:25 +0800

    chore: ignore Android build artifacts and IDE settings

--- modules/expo-ffmpeg/android/build/intermediates/cxx/Debug/33296966/logs/arm64-v8a/build_command_expo-ffmpeg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\Downloads\\video-player\\modules\\expo-ffmpeg\\android\\.cxx\\Debug\\33296966\\arm64-v8a" ^
-  expo-ffmpeg
